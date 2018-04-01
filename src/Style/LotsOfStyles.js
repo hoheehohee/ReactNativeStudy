63ef1aba00008c38b73225c9260abe4e67f7f0a1
hoheehohee <boycat87@gmail.com>
2018-04-01 17:38:17 +0900

    React Native Style

--- src/Style/LotsOfStyles.js
+++ src/Style/LotsOfStyles.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class LotsOfStyles extends Component {
-  renter() {
+  render() {
     return (
       <View>
         <Text style={styles.red}>just red</Text>
